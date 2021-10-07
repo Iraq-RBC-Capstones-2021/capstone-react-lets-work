1034d17002ee3eff4f635c65e84672f944735d9b
noora <noorayaqub44@gmail.com>
2021-10-07 14:25:25 +0300

    bulid(Chakra-UI)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import { wrapper } from "../store";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/provider";
+import theme from "../src/theme/index";
+import "../src/theme/styles.css";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default wrapper.withRedux(App);
