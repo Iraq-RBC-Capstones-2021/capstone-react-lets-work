35e22b93f79468b24037c10a8221923101530cd8
Aryan Majeed <aryanmajid97@gmail.com>
2021-10-08 23:38:31 +0300

    feat(Navbar.js): add links

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,5 +1,42 @@
-import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, Button, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import NextLink from "next/link";
 
 export default function Navbar() {
-  return <Flex>navbar</Flex>;
+  return (
+    <Flex>
+      <Flex pos="fixed" top="1rem" right="1rem" align="center">
+        <Flex display={["none", "none", "flex", "flex"]}>
+          <NextLink href="/" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              Home
+            </Button>
+          </NextLink>
+          <NextLink href="/chat" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              Chat
+            </Button>
+          </NextLink>
+          <NextLink href="/search" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              Search
+            </Button>
+          </NextLink>
+          <NextLink href="/about" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              About
+            </Button>
+          </NextLink>
+        </Flex>
+        <IconButton
+          aria-label="open menu"
+          size="lg"
+          mr={2}
+          icon={<HamburgerIcon />}
+          display={["flex", "flex", "none", "none"]}
+        />
+      </Flex>
+    </Flex>
+  );
 }
