216625a30a9ca4b5a55442410c626f4a7c1fc50e
Haider <haiderlek@gmail.com>
2021-10-08 15:42:27 +0300

    perf(style): add custom Button and checkbox variants

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -1,5 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
-
+import { darken } from "@chakra-ui/theme-tools";
 const fonts = {
   heading: "Roboto",
   body: "Roboto",
@@ -26,7 +26,43 @@ const colors = {
     darker: "#14252f",
   },
 };
+const components = {
+  Button: {
+    variants: {
+      primary: {
+        bg: "primary.main",
+        color: "#ffff",
+        _hover: {
+          bg: darken("primary.main", 5),
+        },
+        _active: {
+          bg: darken("primary.main", 8),
+        },
+      },
+      secondary: {
+        bg: "#2882BC",
+        color: "#ffff",
+        _hover: {
+          bg: darken("#2882BC", 5),
+        },
+        _active: {
+          bg: darken("#2882BC", 8),
+        },
+      },
+    },
+  },
+  Checkbox: {
+    variants: {
+      rounded: {
+        control: {
+          borderRadius: "50%",
+          bg: "#ffff",
+        },
+      },
+    },
+  },
+};
 
-const theme = extendTheme({ colors, fonts });
+const theme = extendTheme({ colors, fonts, components });
 
 export default theme;
