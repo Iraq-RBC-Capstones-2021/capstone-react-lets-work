890743c05d5132a0ea97d8ef1e461c110a55c6f0
Aryan Majeed <aryanmajid97@gmail.com>
2021-10-10 00:24:58 +0300

    feat(Navbar.js): remove layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@fontsource/roboto";
 import "@fontsource/roboto/300.css";
-import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 import { ChakraProvider } from "@chakra-ui/provider";
 
 import { wrapper } from "../store";
@@ -9,9 +9,8 @@ import theme from "../styles/theme/index";
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Navbar />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 };
