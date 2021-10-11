c17216b4f4c421d01eff4c48ca29b3ef453a5694
Aryan Majeed <aryanmajid97@gmail.com>
2021-10-11 17:14:27 +0300

    feat([postid].js): add global background color

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import styles from "../styles/Home.module.css";
 import TopSection from "../components/Home/TopSection";
 export default function Home() {
   return (
-    <div className={styles.app}>
+    <div>
       <TopSection />
     </div>
   );
---
+++ pages/posts/[postId]/index.js
@@ -0,0 +1,10 @@
+import { Container, Box } from "@chakra-ui/react";
+
+export default function index() {
+  return (
+    <Container maxW="xl" centerContent>
+      There are many benefits to a joint design and development system. Not only
+      does it bring benefits to the design team.
+    </Container>
+  );
+}
