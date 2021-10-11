97e14c3285ea45fd4c4cb8fb3a465e2184d5a200
Aryan Majeed <aryanmajid97@gmail.com>
2021-10-11 18:47:55 +0300

    feat([postId].js): add contents

--- pages/posts/[postId]/index.js
+++ pages/posts/[postId]/index.js
@@ -1,10 +1,42 @@
-import { Container, Box } from "@chakra-ui/react";
+import Image from "next/image";
+import { Box, Center, Heading, Text, Stack, Avatar } from "@chakra-ui/react";
 
 export default function index() {
   return (
-    <Container maxW="xl" centerContent>
-      There are many benefits to a joint design and development system. Not only
-      does it bring benefits to the design team.
-    </Container>
+    <Center p="6">
+      <Box
+        maxW="780px"
+        w="full"
+        bg="white"
+        rounded="15px"
+        p="6"
+        overflow="hidden"
+      >
+        <Stack>
+          <Heading color="gray.700" fontSize="2xl" fontFamily="body">
+            Rocket Project
+          </Heading>
+          <Text color={"gray.500"}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Lorem ipsum dolor sit amet consectetur adipisicing
+          </Text>
+        </Stack>
+        <Box height="40vw" maxH="600px" mb="6" pos="relative">
+          <Image src="/images/movies-project.svg" alt="" layout="fill" />
+        </Box>
+        <Stack mt="6" direction="row" spacing="4" align="center">
+          <Avatar
+            src="https://avatars0.githubusercontent.com/u/1164541?v=4"
+            alt="Author"
+          />
+          <Stack direction={"column"} spacing={0} fontSize={"sm"}>
+            <Text fontWeight={600}>Achim Rolle</Text>
+            <Text color={"gray.500"}>Content Creator, Web Developer</Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
   );
 }
