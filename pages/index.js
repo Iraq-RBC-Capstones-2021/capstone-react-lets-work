a633ad249a7a4ab48e9a8b7e75a363acb90f3af8
noora <noorayaqub44@gmail.com>
2021-10-11 15:44:27 +0300

    feat(comment component created)

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import styles from "../styles/Home.module.css";
 import TopSection from "../components/Home/TopSection";
+import { Comment } from "../components/Comment";
 export default function Home() {
   return (
     <div className={styles.app}>
       <TopSection />
+      <Comment />
     </div>
   );
 }
