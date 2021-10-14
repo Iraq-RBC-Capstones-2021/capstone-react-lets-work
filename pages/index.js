ac4ae05ef6f82ed2adb7711be0a9461c745ff277
HaiderAltufaily <82726695+HaiderAltufaily@users.noreply.github.com>
2021-10-14 15:09:47 +0300

    Add post cards and postsList components (#35)
    * feat(postCard&postList): add postCard and list components
    
    * feat(postList): add different post lists to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import styles from "../styles/Home.module.css";
 import TopSection from "../components/Home/TopSection";
+import PostList from "../components/Home/posts/PostList";
+const postSample = [
+  {
+    createdAt: "posted 12/12/2021",
+    description:
+      "Hidden universe revealed in stunning first images from German telescope",
+    imageURL: "https://source.unsplash.com/random",
+    isCompleted: "false",
+    likes: ["userId", "userId"],
+    tags: ["Design", "Rocket", "Telescope"],
+    title: "Coding challenge",
+    userId: "",
+    postId: "1",
+  },
+
+  {
+    createdAt: "12/12/2021",
+    description:
+      "Hidden universe revealed in stunning first images from German telescope",
+    imageURL: "https://source.unsplash.com/random/2",
+    isCompleted: "false",
+    likes: ["userId"],
+    tags: ["Design", "Rocket", "Telescope"],
+    title: "Coding challenge",
+    userId: "",
+    postId: "2",
+  },
+
+  {
+    createdAt: "12/12/2021",
+    description:
+      "Hidden universe revealed in stunning first images from German telescope Hidden universe revealed in stunning first images from German telescope",
+    imageURL: "",
+    isCompleted: "false",
+    likes: ["userId", "userId", "userId", "userId"],
+    tags: ["Design", "Rocket", "Telescope"],
+    title: "Health Care System",
+    userId: "",
+    postId: "3",
+  },
+];
 export default function Home() {
   return (
     <div className={styles.app}>
       <TopSection />
+      <PostList list="Top Projects" posts={postSample} />
+      <PostList list="Favorite Projects" posts={postSample} />
+      <PostList list="New Projects" posts={postSample} />
     </div>
   );
 }
