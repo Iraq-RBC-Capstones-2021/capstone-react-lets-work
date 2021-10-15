07e000a2af4aaaddf622b3713623934cd842c96a
Haider <haiderlek@gmail.com>
2021-10-15 16:37:36 +0300

    feat(_app.js): keep track of authentication status

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,26 @@ import { ChakraProvider } from "@chakra-ui/provider";
 import { appWithTranslation } from "next-i18next";
 import { wrapper } from "../store";
 import theme from "../styles/theme/index";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth } from "../firebase/firebase";
+import { setIsLoggedIn } from "../store/auth/authSlice";
+import { useDispatch } from "react-redux";
 
 const App = ({ Component, pageProps }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setIsLoaded(true);
+
+      user ? dispatch(setIsLoggedIn(true)) : dispatch(setIsLoggedIn(false));
+    });
+  }, [dispatch]);
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
-      <Component {...pageProps} />
+      {isLoaded && <Component {...pageProps} />}
     </ChakraProvider>
   );
 };
