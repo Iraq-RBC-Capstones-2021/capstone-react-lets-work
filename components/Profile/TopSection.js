f1185c3d838ac4957baa4d6c0355a4fef912d54b
noora <noorayaqub44@gmail.com>
2021-10-16 21:31:23 +0300

    fix(top section)mobile and tab design)

--- components/Profile/TopSection.js
+++ components/Profile/TopSection.js
@@ -10,6 +10,7 @@ import {
   Tag,
   IconButton,
   Link,
+  Flex,
 } from "@chakra-ui/react";
 import { SiGmail, SiInstagram, SiFacebook, SiLinkedin } from "react-icons/si";
 
@@ -25,93 +26,121 @@ function TopSection() {
 
   return (
     <Stack>
-      <Box bg="secondary.main" px="150px" py="68px">
-        <HStack spacing={50} alignContent="center" display={{ md: "flex" }}>
-          <Stack>
-            <Image
-              borderRadius="full"
-              width={{ md: 40 }}
-              src="https://bit.ly/sage-adebayo"
-              alt="Segun Adebayo"
-            />
-          </Stack>
-          <Stack>
-            <Heading fontSize={{ base: "25px", md: "27px" }}>
-              {user.username}
-            </Heading>
-            <Text color="gray.500" fontWeight="light">
-              {" "}
-              {user.bio}
-            </Text>
-            <HStack align="center" spacing={0} display={{ md: "flex" }}>
-              <IconButton
-                as={Link}
-                href={user.social.email}
-                icon={<SiGmail />}
+      <Flex
+        justify="space-between"
+        direction={["column", "column", "row", "row"]}
+        align={{ base: "flex-start", md: "center" }}
+        bg="secondary.main"
+      >
+        <Box px={["50px", "80px", "150px"]} py={["30px", "50px", "68px"]}>
+          <Flex
+            direction={["column", "column", "row", "row"]}
+            justify="space-around"
+            align={{ base: "flex-start", md: "center" }}
+          >
+            <Stack>
+              <Image
+                borderRadius="full"
+                objectFit="cover"
+                boxSize={{ base: "8rem", md: "10rem", lg: "15rem" }}
+                src="https://bit.ly/sage-adebayo"
+                alt="Segun Adebayo"
               />
-              <IconButton
-                as={Link}
-                href={user.social.instagram}
-                icon={<SiInstagram />}
-              />
-              <IconButton
-                as={Link}
-                href={user.social.facebook}
-                icon={<SiFacebook />}
-              />
-              <IconButton
-                as={Link}
-                href={user.social.linkedIn}
-                icon={<SiLinkedin />}
-              />
-            </HStack>
-            <Text color="gray.500" fontWeight="light">
-              {" "}
-              {user.about}
-            </Text>
-            <VStack align="flex-start">
-              <Button variant="primary">Message</Button>
-            </VStack>
-          </Stack>
-        </HStack>
-      </Box>
-      <Box paddingX="150px" paddingY="40px" margin="25px">
-        <Heading paddingBottom="20px" fontSize={{ base: "25px", md: "27px" }}>
-          Interests
-        </Heading>
-        <HStack spacing={4} display={{ md: "flex" }}>
-          {user.interests.map((interest) => (
-            <Tag size="lg" variant="subtle" key={interest}>
-              {interest}
-            </Tag>
-          ))}
-        </HStack>
-      </Box>
-      <Box paddingX="150px" paddingBottom="50px" spacing={25}>
-        <Heading paddingBottom="20px" fontSize={{ base: "25px", md: "27px" }}>
-          About
-        </Heading>
-        <Text>
-          A mote of dust suspended in a sunbeam Sea of Tranquility vanquish the
-          impossible shores of the cosmic ocean a billion trillion another
-          world. Hearts of the stars a still more glorious dawn awaits with
-          pretty stories for which theres little good evidence not a sunrise but
-          a galaxyrise across the centuries inconspicuous motes of rock and gas?
-          Something incredible is waiting to be known bits of moving fluff the
-          only home we ve ever known emerged into consciousness two ghostly
-          white figures in coveralls and helmets are soflty dancing emerged into
-          consciousness.
-        </Text>
-      </Box>
-      <Box bg="secondary.main" px="150px" py="35px">
-        <Heading paddingBottom="20px" fontSize={{ base: "25px", md: "27px" }}>
-          Skills & Hobbies
-        </Heading>
+            </Stack>
+            <Stack mx="8">
+              <Heading fontSize={{ base: "25px", md: "27px" }}>
+                {user.username}
+              </Heading>
+              <Text color="gray.500" fontWeight="light">
+                {" "}
+                {user.bio}
+              </Text>
+              <HStack align="center" spacing={0} display={{ md: "flex" }}>
+                <IconButton
+                  as={Link}
+                  href={user.social.email}
+                  icon={<SiGmail />}
+                />
+                <IconButton
+                  as={Link}
+                  href={user.social.instagram}
+                  icon={<SiInstagram />}
+                />
+                <IconButton
+                  as={Link}
+                  href={user.social.facebook}
+                  icon={<SiFacebook />}
+                />
+                <IconButton
+                  as={Link}
+                  href={user.social.linkedIn}
+                  icon={<SiLinkedin />}
+                />
+              </HStack>
+              <Text color="gray.500" fontWeight="light">
+                {" "}
+                {user.about}
+              </Text>
+              <VStack align="flex-start">
+                <Button variant="primary">Message</Button>
+              </VStack>
+            </Stack>
+          </Flex>
+        </Box>
+      </Flex>
+      <Flex
+        direction={["column", "column", "row", "row"]}
+        align={{ base: "flex-start", md: "center" }}
+      >
+        <Box px={["50px", "100px", "150px"]} paddingY="40px">
+          <Heading paddingBottom="20px" fontSize={{ base: "25px", md: "27px" }}>
+            Interests
+          </Heading>
+          <HStack justify="space-around" display={{ md: "flex" }}>
+            {user.interests.map((interest) => (
+              <Tag size="lg" variant="subtle" key={interest}>
+                {interest}
+              </Tag>
+            ))}
+          </HStack>
+        </Box>
+      </Flex>
+      <Flex
+        direction={["column", "column", "row", "row"]}
+        align={{ base: "flex-start", md: "flex-start" }}
+      >
+        <Box px={["50px", "100px", "150px"]} paddingBottom="50px" spacing={25}>
+          <Heading paddingBottom="20px" fontSize={{ base: "25px", md: "27px" }}>
+            About
+          </Heading>
+          <Text>
+            A mote of dust suspended in a sunbeam Sea of Tranquility vanquish
+            the impossible shores of the cosmic ocean a billion trillion another
+            world. Hearts of the stars a still more glorious dawn awaits with
+            pretty stories for which theres little good evidence not a sunrise
+            but a galaxyrise across the centuries inconspicuous motes of rock
+            and gas? Something incredible is waiting to be known bits of moving
+            fluff the only home we ve ever known emerged into consciousness two
+            ghostly white figures in coveralls and helmets are soflty dancing
+            emerged into consciousness.
+          </Text>
+        </Box>
+      </Flex>
+      <Flex
+        direction={["column", "column", "row", "row"]}
+        align={{ base: "flex-start", md: "flex-start" }}
+        bg="secondary.main"
+      >
+        <Box px={["50px", "100px", "150px"]} py="35px">
+          <Heading paddingBottom="20px" fontSize={{ base: "25px", md: "27px" }}>
+            Skills & Hobbies
+          </Heading>
 
-        {user.skills_hobbies.map((skill) => {
-          return <Text key={skill}>{skill}</Text>;
-        })}
-      </Box>
+          {user.skills_hobbies.map((skill) => {
+            return <Text key={skill}>{skill}</Text>;
+          })}
+        </Box>
+      </Flex>
     </Stack>
   );
 }
