b9f0e27539f709c06cb730284e76ef16d5436bc5
Aryan Majeed <aryanmajid97@gmail.com>
2021-10-16 19:14:53 +0300

    feat(ChakraTextarea.js): fix position

--- components/Shared/ChakraTextarea.js
+++ components/Shared/ChakraTextarea.js
@@ -1,12 +1,12 @@
 import { ErrorMessage, Field } from "formik";
-import { Textarea } from "@chakra-ui/react";
+import { Textarea, VStack } from "@chakra-ui/react";
 import TextError from "./TextError";
 function ChakraTextarea({ name, ...rest }) {
   return (
-    <>
+    <VStack>
       <Field as={Textarea} name={name} {...rest} />
       <ErrorMessage name={name} component={TextError} />
-    </>
+    </VStack>
   );
 }
 
