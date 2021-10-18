a3f490d69417ed6ec5e7ad2df85ff7cfc6f740ff
Haider <haiderlek@gmail.com>
2021-10-18 22:46:03 +0300

    fix(navbar): fix styles after signing in

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,33 @@ import {
   Text,
   HStack,
   useToast,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
+  Avatar,
+  Icon,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  Stack,
+  Box,
 } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { BiLogOut } from "react-icons/bi";
+import { FiSettings } from "react-icons/fi";
+import { CgProfile } from "react-icons/cg";
 import NextLink from "next/link";
 import NotificationsList from "./Notifications/NotificationsList";
 import { auth } from "../firebase/firebase";
@@ -50,31 +75,157 @@ export default function Navbar() {
     dispatch(handleSignOut(auth.currentUser?.uid));
   }
   return (
-    // This is for testing purposes feel free to remove:
-    <Flex
-      align="center"
-      bgColor="offWhite"
-      justify={auth.currentUser ? "space-around" : "end"}
-    >
-      {isLoggedIn && auth.currentUser && (
-        <HStack>
-          <Text color="secondary.main" fontSize="2xl">
-            {" "}
-            Welcome{" "}
-          </Text>
-          <Text fontSize="2xl"> {auth.currentUser.displayName} </Text>
-          <Button variant="primary" onClick={signOutHandler}>
-            Log out
-          </Button>
+    <Flex align="center" justify="end" bgColor="offWhite">
+      <Flex align="center" display={["flex", "flex", "none", "none"]} mr="auto">
+        <NotificationsList />
+      </Flex>
+      <HStack
+        justify={{ base: "end", md: "" }}
+        minW="50%"
+        top="1rem"
+        right="1rem"
+        align="center"
+        spacing="10"
+      >
+        <HStack
+          w="100%"
+          align="center"
+          display={["none", "none", "flex", "flex"]}
+          justify="end"
+          spacing="3"
+        >
+          <NextLink href="/" passHref>
+            <Button as="a" variant="ghost" aria-label="Home">
+              Home
+            </Button>
+          </NextLink>
+          <NextLink href="/chat" passHref>
+            <Button as="a" variant="ghost" aria-label="Home">
+              Chat
+            </Button>
+          </NextLink>
+          <NextLink href="/search" passHref>
+            <Button as="a" variant="ghost" aria-label="Home">
+              Search
+            </Button>
+          </NextLink>
+          <NextLink href="/about" passHref>
+            <Button as="a" variant="ghost" aria-label="Home">
+              About
+            </Button>
+          </NextLink>
         </HStack>
-      )}
-      {/* {  ends here  } */}
-      <Flex justify="end" bgColor="offWhite">
-        <Flex display={["flex", "flex", "none", "none"]} mr="auto">
+        <HStack
+          align="center"
+          display={["none", "none", "flex", "flex"]}
+          spacing="3"
+        >
+          {auth.currentUser ? (
+            <Popover>
+              <PopoverTrigger>
+                <Button
+                  rightIcon={<ChevronDownIcon />}
+                  as={Button}
+                  bg="transparent"
+                  _hover={{ bg: "transparent" }}
+                  _focus={{ boxShadow: "none" }}
+                  _active={{}}
+                  p="0"
+                >
+                  <HStack>
+                    <Avatar size="sm" src={auth.currentUser?.photoURL} />
+                    <Text> {auth.currentUser.displayName} </Text>
+                  </HStack>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                _focus={{ boxShadow: "lg" }}
+                boxShadow="lg"
+                ml={{ base: "10px", md: "0px" }}
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader
+                  cursor="pointer"
+                  _hover={{ bg: "gray.100" }}
+                  mt="3"
+                >
+                  <NextLink href="/account">
+                    <HStack>
+                      <Icon h="5" w="5" as={CgProfile} />
+                      <Text>Profile </Text>
+                    </HStack>
+                  </NextLink>
+                </PopoverHeader>
+                <PopoverBody cursor="pointer" _hover={{ bg: "gray.100" }}>
+                  <NextLink href="/setting">
+                    <HStack>
+                      <Icon h="5" w="5" as={FiSettings} />
+                      <Text>Account Settings</Text>
+                    </HStack>
+                  </NextLink>
+                </PopoverBody>
+                <PopoverFooter
+                  onClick={signOutHandler}
+                  cursor="pointer"
+                  _hover={{ bg: "gray.100" }}
+                >
+                  <HStack>
+                    {" "}
+                    <Icon h="5" w="5" as={BiLogOut} /> <Text>Logout</Text>{" "}
+                  </HStack>{" "}
+                </PopoverFooter>
+              </PopoverContent>
+            </Popover>
+          ) : (
+            <NextLink href="/signup" passHref>
+              <Button as="a" variant="ghost" aria-label="Home">
+                Sign Up
+              </Button>
+            </NextLink>
+          )}
+
           <NotificationsList />
-        </Flex>
-        <Flex top="1rem" right="1rem" align="center">
-          <Flex display={["none", "none", "flex", "flex"]}>
+        </HStack>
+        <IconButton
+          aria-label="open menu"
+          size="lg"
+          mr="2"
+          marginY="2"
+          icon={<HamburgerIcon />}
+          display={["flex", "flex", "none", "none"]}
+          onClick={() => setDisplay("flex")}
+        />
+      </HStack>
+      <Flex display={["flex", "flex", "none", "none"]}>
+        <Flex
+          w="100vw"
+          bgColor="offWhite"
+          zIndex={20}
+          minHeight="100%"
+          pos="fixed"
+          top="0"
+          left="0"
+          overflow="auto"
+          flexDir="column"
+          display={display}
+        >
+          <Flex justify="flex-end">
+            <IconButton
+              mt="2"
+              mr="2"
+              aria-label="Close menu"
+              size="lg"
+              icon={<CloseIcon />}
+              onClick={() => setDisplay("none")}
+            />
+          </Flex>
+          <Flex
+            height="100%"
+            flexDir="column"
+            align="center"
+            onClick={() => setDisplay("none")}
+          >
             <NextLink href="/" passHref>
               <Button as="a" variant="ghost" aria-label="Home" my="2" w="100%">
                 Home
@@ -95,62 +246,32 @@ export default function Navbar() {
                 About
               </Button>
             </NextLink>
-            <NextLink href={auth.currentUser ? "/account" : "/signup"} passHref>
-              <Button
-                as="a"
-                variant="ghost"
-                aria-label="Home"
-                my="2"
-                w="100%"
-                ml="14"
-                mr="2"
-              >
-                {auth.currentUser ? "Account" : "Sign Up"}
-              </Button>
-            </NextLink>
-
-            <NotificationsList />
-          </Flex>
-          <IconButton
-            aria-label="open menu"
-            size="lg"
-            mr="2"
-            marginY="2"
-            icon={<HamburgerIcon />}
-            display={["flex", "flex", "none", "none"]}
-            onClick={() => setDisplay("flex")}
-          />
-        </Flex>
-        <Flex display={["flex", "flex", "none", "none"]}>
-          <Flex
-            w="100vw"
-            bgColor="offWhite"
-            zIndex={20}
-            minHeight="100%"
-            pos="fixed"
-            top="0"
-            left="0"
-            overflow="auto"
-            flexDir="column"
-            display={display}
-          >
-            <Flex justify="flex-end">
-              <IconButton
-                mt="2"
-                mr="2"
-                aria-label="Close menu"
-                size="lg"
-                icon={<CloseIcon />}
-                onClick={() => setDisplay("none")}
-              />
-            </Flex>
-            <Flex
-              height="100%"
-              flexDir="column"
-              align="center"
-              onClick={() => setDisplay("none")}
-            >
-              <NextLink href="/" passHref>
+            {auth.currentUser ? (
+              <Stack>
+                <NextLink href="/account">
+                  <Button
+                    as="a"
+                    variant="ghost"
+                    aria-label="Home"
+                    my="2"
+                    w="100%"
+                  >
+                    Profile
+                  </Button>
+                </NextLink>
+                <Button
+                  as="a"
+                  variant="ghost"
+                  aria-label="Home"
+                  my="2"
+                  w="100%"
+                  onClick={signOutHandler}
+                >
+                  Logout
+                </Button>
+              </Stack>
+            ) : (
+              <NextLink href="/signup">
                 <Button
                   as="a"
                   variant="ghost"
@@ -158,55 +279,10 @@ export default function Navbar() {
                   my="2"
                   w="100%"
                 >
-                  Home
+                  Sign Up
                 </Button>
               </NextLink>
-              <NextLink href="/chat" passHref>
-                <Button
-                  as="a"
-                  variant="ghost"
-                  aria-label="Home"
-                  my="2"
-                  w="100%"
-                >
-                  Chat
-                </Button>
-              </NextLink>
-              <NextLink href="/search" passHref>
-                <Button
-                  as="a"
-                  variant="ghost"
-                  aria-label="Home"
-                  my="2"
-                  w="100%"
-                >
-                  Search
-                </Button>
-              </NextLink>
-              <NextLink href="/about" passHref>
-                <Button
-                  as="a"
-                  variant="ghost"
-                  aria-label="Home"
-                  my="2"
-                  w="100%"
-                >
-                  About
-                </Button>
-              </NextLink>
-              <NextLink href="#" passHref>
-                <Button
-                  as="a"
-                  variant="ghost"
-                  aria-label="Home"
-                  my="2"
-                  w="100%"
-                  onClick={authenticate}
-                >
-                  {signedIn ? "Account" : "Sign Up"}
-                </Button>
-              </NextLink>
-            </Flex>
+            )}
           </Flex>
         </Flex>
       </Flex>
--- firebase/firebaseErrors.js
+++ firebase/firebaseErrors.js
@@ -1,9 +1,9 @@
 export function firebaseErrors(error) {
   switch (error) {
     case "Firebase: Error (auth/user-not-found).":
-      return "There is no such a user with this email, Sign Up first";
+      return "Incorrect Email";
     case "Firebase: Error (auth/email-already-in-use).":
-      return "Account already exists, use another email or Log In";
+      return "Incorrect Email";
     case "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).":
       return "Too many attempts, try again later.";
     case "Firebase: Error (auth/wrong-password).":
