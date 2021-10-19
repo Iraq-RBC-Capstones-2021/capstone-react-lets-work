18c5775aa0477b39bab606f5e9d2e7974fc21bba
noora <noorayaqub44@gmail.com>
2021-10-19 18:38:40 +0300

    fix(add project)

--- pages/AddProject.js
+++ pages/AddProject.js
@@ -10,8 +10,11 @@ import {
   IconButton,
 } from "@chakra-ui/react";
 import { FiSend, FiTrash2, FiImage, FiLink2 } from "react-icons/fi";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 import React from "react";
+import ChakraInput from "../components/Shared/ChakraInput";
 
 export default function AddProject() {
   const toast = useToast();
@@ -43,80 +46,121 @@ export default function AddProject() {
     toastIdRef.current = toast({ description: "tag added" });
   }
   return (
-    <Stack align="center" justify="center" bg="secondary.main" height="93vh">
-      <Box
-        mx={{ base: "3" }}
-        bg="white"
-        color="blue.300"
-        borderRadius="xl"
-        p={["5", "6", "8"]}
-        h={{ base: "100%", md: "auto" }}
-        w={{ md: "auto", base: "100%" }}
-      >
-        <Stack spacing={3}>
-          <VStack
-            alignContent="space-evenly"
-            align="flex-start"
-            justify="flex-start"
+    <Formik
+      initialValues={{ projectName: "", description: "", tags: "" }}
+      validationSchema={Yup.object({
+        projectName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        description: Yup.string()
+          .max(200, "Must be 200 characters or less")
+          .required("Required"),
+        tags: Yup.string().email("Invalid email address").required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <Stack
+          align="center"
+          justify="center"
+          bg="secondary.main"
+          height="70vh"
+        >
+          <Box
+            mx={{ base: "3" }}
+            bg="white"
+            color="blue.300"
+            borderRadius="xl"
+            p={["5", "6", "8"]}
+            h={{ base: "100%", md: "auto" }}
+            w={{ md: "auto", base: "100%" }}
           >
-            <Heading
-              as="h5"
-              fontSize={{ base: "15px", md: "10px" }}
-              color="black"
-            >
-              New Project/idea
-            </Heading>
-            <Input
-              placeholder="Project name"
-              variant="flushed"
-              color="blue.500"
-              fontSize={{ base: "11px", md: "10px" }}
-            />
-            <Input
-              placeholder="Description"
-              variant="flushed"
-              fontSize={{ base: "10px", md: "10px" }}
-            />
-          </VStack>
-          <VStack align="flex-start" justify="flex-start">
-            <Heading
-              as="h5"
-              fontSize={{ base: "15px", md: "10px" }}
-              color="black"
-            >
-              Tags
-            </Heading>
-            <Input
-              placeholder="Write your categories here.."
-              variant="flushed"
-              fontSize={{ base: "11px", md: "10px" }}
-            />
+            <Stack spacing={3}>
+              <VStack
+                alignContent="space-evenly"
+                align="flex-start"
+                justify="flex-start"
+              >
+                <Heading
+                  as="h5"
+                  fontSize={{ base: "22px", md: "18px" }}
+                  color="black"
+                >
+                  New Project/idea
+                </Heading>
+                {/* <Input
+                  placeholder="Project name"
+                  variant="flushed"
+                  color="blue.500"
+                  fontSize={{ base: "18px", md: "15px" }}
+                /> */}
+                <ChakraInput
+                  placeholder="Project name"
+                  variant="flushed"
+                  color="blue.500"
+                  fontSize={{ base: "18px", md: "15px" }}
+                  name="projectName"
+                  w="100%"
+                />
+                {/* <Input
+                  placeholder="Description"
+                  variant="flushed"
+                  fontSize={{ base: "18px", md: "15px" }}
+                /> */}
+                <ChakraInput
+                  placeholder="Description"
+                  variant="flushed"
+                  fontSize={{ base: "18px", md: "15px" }}
+                  name="description"
+                  w="100%"
+                />
+              </VStack>
+              <VStack align="flex-start" justify="flex-start">
+                <Heading
+                  as="h5"
+                  fontSize={{ base: "22px", md: "18px" }}
+                  color="black"
+                >
+                  Tags
+                </Heading>
+                <Input
+                  placeholder="Write your categories here.."
+                  variant="flushed"
+                  fontSize={{ base: "18px", md: "15px" }}
+                />
 
-            <HStack wrap="wrap" spacing={3} flexShrink={0}>
-              {tags.map((tag) => {
-                return (
-                  <Button
-                    fontSize={{ base: "12px", md: "10px" }}
-                    key={tag}
-                    onClick={addToast}
-                    type="button"
-                    variant="secondary.main"
-                  >
-                    {tag}
-                  </Button>
-                );
-              })}
-            </HStack>
-          </VStack>
+                <HStack wrap="wrap" spacing={3} flexShrink={0}>
+                  {tags.map((tag) => {
+                    return (
+                      <Button
+                        fontSize={{ base: "15px", md: "16px" }}
+                        key={tag}
+                        onClick={addToast}
+                        type="button"
+                        variant="secondary.main"
+                      >
+                        {tag}
+                      </Button>
+                    );
+                  })}
+                </HStack>
+              </VStack>
 
-          <Stack isInline spacing={2} wrap="wrap">
-            <IconButton icon={<FiImage />} />
-            <IconButton icon={<FiLink2 />} />
-            <IconButton icon={<FiTrash2 />} />
-            <IconButton icon={<FiSend />} />
-          </Stack>
+              <Stack isInline spacing={2} wrap="wrap">
+                <IconButton icon={<FiImage />} />
+                <IconButton icon={<FiLink2 />} />
+                <IconButton icon={<FiTrash2 />} />
+                <IconButton icon={<FiSend />} type="submit" />
+              </Stack>
+            </Stack>
+          </Box>
         </Stack>
-      </Box>
-    </Stack>
+      </Form>
+    </Formik>
   );
 }
