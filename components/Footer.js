5a09e57f99b7daec1391ad62fe2218a40f9a8cb5
Noora Yaqub <72706003+NooraYaqub44@users.noreply.github.com>
2021-10-29 15:16:44 +0300

    Noora yaqub44/issue49 (#72)
    * feat(footer):emailjs added)
    
    * feat(footer):form emailjs added
    
    * feat(footer):formik validation
    
    * fix(form-footer): toast added
    
    * fix(footer contact us form):toast fixed
    
    * fix(footer):build fixed
    
    * fix(footer):textarea bg color

--- components/Footer.js
+++ components/Footer.js
@@ -14,10 +14,61 @@ import {
   Input,
   Button,
   InputGroup,
+  FormControl,
 } from "@chakra-ui/react";
 import { PhoneIcon, AtSignIcon } from "@chakra-ui/icons";
-import Image from "next/image";
+import React, { useRef, useState, useEffect } from "react";
+import { useToast } from "@chakra-ui/toast";
+import emailjs from "emailjs-com";
+import { Formik, Form } from "formik";
+import ChakraInput from "./Shared/ChakraInput";
+import * as Yup from "yup";
+
 function Footer() {
+  const form = useRef();
+  const [status, setStatus] = useState("");
+  const toast = useToast();
+  useEffect(() => {
+    if (status === "error") {
+      toast({
+        title: "Failed to send email",
+        position: "top",
+        duration: "3000",
+        status: "error",
+        variant: "subtle",
+      });
+      setStatus("");
+    }
+    if (status === "success") {
+      toast({
+        title: "Email sent",
+        position: "top",
+        duration: "3000",
+        status: "success",
+        variant: "subtle",
+      });
+      setStatus("");
+    }
+  }, [status, toast]);
+  const sendEmail = (values, onSubmitProps) => {
+    emailjs
+      .send(
+        "service_th0e88m",
+        "template_rd6cisi",
+        values,
+        "user_KhJ47OC9g6wyXA1O4IWuR"
+      )
+      .then(
+        (result) => {
+          setStatus("success");
+        },
+        (error) => {
+          setStatus("error");
+        }
+      );
+    onSubmitProps.resetForm();
+  };
+
   return (
     <Flex
       bg="#1C3341"
@@ -101,21 +152,60 @@ function Footer() {
           <Heading fontSize={{ base: "35px", md: "30px" }}>Contact us</Heading>
           <Text>Send us messages</Text>
           <InputGroup w={{ base: "15rem", lg: "auto" }}>
-            <Stack
-              width={("sm", "sm", "sm", "xs")}
-              display={["flow", "flow", "flex"]}
+            <Formik
+              initialValues={{ user_name: "", email: "", message: "" }}
+              validationSchema={Yup.object({
+                user_name: Yup.string()
+                  .max(15, "Must be 15 characters or less")
+                  .required("Required"),
+                message: Yup.string()
+                  .max(200, "Must be 200 characters or less")
+                  .required("Required"),
+                email: Yup.string()
+                  .email("Please enter a valid email")
+                  .required("Please enter your email address"),
+              })}
+              onSubmit={sendEmail}
             >
-              <Input variant="filled" placeholder="Name" />
-              <Input variant="filled" placeholder="Email" />
-              <Input
-                as={Textarea}
-                variant="filled"
-                placeholder="Your Message"
-              />
-              <Button bg="blue.400" variant="primary" width="100%">
-                Submit
-              </Button>
-            </Stack>
+              <Form>
+                <Stack
+                  width={("sm", "sm", "sm", "xs")}
+                  display={["flow", "flow", "flex"]}
+                >
+                  <ChakraInput
+                    color="black"
+                    variant="primary"
+                    placeholder="Name"
+                    type="text"
+                    name="user_name"
+                  />
+                  <ChakraInput
+                    color="black"
+                    variant="primary"
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                  />
+                  <ChakraInput
+                    color="black"
+                    bg="white"
+                    as={Textarea}
+                    variant="primary"
+                    placeholder="Your Message"
+                    name="message"
+                  />
+                  <Button
+                    bg="blue.400"
+                    variant="primary"
+                    width="100%"
+                    type="submit"
+                    value="Send"
+                  >
+                    Submit
+                  </Button>
+                </Stack>
+              </Form>
+            </Formik>
           </InputGroup>
         </Stack>
       </Box>
