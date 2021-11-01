d5a55434fda3cbde4a1bde526d6eaa406d2b0be3
noora <noorayaqub44@gmail.com>
2021-11-01 22:42:29 +0300

    fix(nprogress):fixing the bugs

--- pages/_app.js
+++ pages/_app.js
@@ -47,7 +47,7 @@ const App = ({ Component, pageProps }) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
           integrity="sha512-DanfxWBasQtq+RtkNAEDTdX4Q6BPCJQ/kexi/RftcP0BcA4NIJPSi7i31Vl+Yl5OCfgZkdJmCqz+byTOIIRboQ=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
         />
       </Head>
