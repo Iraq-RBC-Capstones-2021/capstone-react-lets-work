6cc152d67b1472005a4eb502f68f83c76dff0d2b
noora <noorayaqub44@gmail.com>
2021-11-02 21:35:18 +0300

    fix(bug)

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,8 @@ import Head from "next/head";
 import Link from "next/link";
 import NProgress from "nprogress";
 
+NProgress.configure({ showSpinner: false });
+
 const App = ({ Component, pageProps }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
